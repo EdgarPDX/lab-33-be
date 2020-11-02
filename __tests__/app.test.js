2b4c3cb7f1d1318d41342ef70c5675f9374539e2
EdgarPDX <66703437+EdgarPDX@users.noreply.github.com>
2020-11-01 19:28:26 -0800

    changed some names in class and deleted some properties on class

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -15,27 +15,21 @@ describe('lab-33-be routes', () => {
       .post('/api/v1/entries')
       .send({
         journalDate:'11/1/2020',
-        gratefulFor: 'Being alive',
-        favoritePart: 'Eating breakfast',
+        gratefulEntry: 'Being alive',
+        favoriteEntry: 'Eating breakfast',
         stressRating: 5,
         stressDescription: 'Woke up late and pushed up schedule',
-        stressManagement: 'I will get to sleep earlier',
-        procrastinationRating: 10,
-        procrastinationReasoning: 'Could not come up with topic',
-        procrastinationManagement: 'Will try to schedule time everyday to work on project'      
+        stressManagement: 'I will get to sleep earlier'     
       })
       .then(res => {
         expect(res.body).toEqual({
           id: expect.any(String),
           journalDate:'11/1/2020',
-          gratefulFor: 'Being alive',
-          favoritePart: 'Eating breakfast',
+          gratefulEntry: 'Being alive',
+          favoriteEntry: 'Eating breakfast',
           stressRating: 5,
           stressDescription: 'Woke up late and pushed up schedule',
-          stressManagement: 'I will get to sleep earlier',
-          procrastinationRating: 10,
-          procrastinationReasoning: 'Could not come up with topic',
-          procrastinationManagement: 'Will try to schedule time everyday to work on project'      
+          stressManagement: 'I will get to sleep earlier'      
         });
       });
   });
--- sql/setup.sql
+++ sql/setup.sql
@@ -3,12 +3,9 @@ DROP TABLE IF EXISTS entries;
 CREATE TABLE entries (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     journal_date TEXT NOT NULL,
-    grateful_for TEXT NOT NULL,
-    favorite_part TEXT NOT NULL,
+    grateful_entry TEXT NOT NULL,
+    favorite_entry TEXT NOT NULL,
     stress_rating INT NOT NULL,
     stress_description TEXT NOT NULL,
-    stress_management TEXT NOT NULL,
-    procrastination_rating INT NOT NULL,
-    procrastination_reasoning TEXT NOT NULL,
-    procrastination_management TEXT NOT NULL
+    stress_management TEXT NOT NULL
 )
